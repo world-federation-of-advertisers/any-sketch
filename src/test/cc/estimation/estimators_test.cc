512bdc4815f38cbbb5cca1c993fc665b4462900d
renjiez <renjiez@google.com>
2022-05-19 03:59:53 +0000

    Fix estimation test

--- src/test/cc/estimation/estimators_test.cc
+++ src/test/cc/estimation/estimators_test.cc
@@ -26,7 +26,7 @@
 #include "common_cpp/fingerprinters/fingerprinters.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "math/expint.h"
+#include "expint.h"
 
 namespace wfa::estimation {
 namespace {
@@ -47,9 +47,9 @@ uint64_t GetExpectedActiveRegisterCount(double decay_rate,
   if (cardinality == 0) return 0;
   double exp_rate = std::exp(decay_rate);
   double t = cardinality / static_cast<double>(num_of_total_registers);
-  double negative_term = -wfa::math::expint((-decay_rate * t) / (exp_rate - 1));
+  double negative_term = -llvm::expint((-decay_rate * t) / (exp_rate - 1));
   double positive_term =
-      wfa::math::expint((-decay_rate * exp_rate * t) / (exp_rate - 1));
+      llvm::expint((-decay_rate * exp_rate * t) / (exp_rate - 1));
   return (1 - (negative_term + positive_term) / decay_rate) *
          num_of_total_registers;
 }
