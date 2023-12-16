082ccf79bc38da76d454a7a8e40a548dc26289f8
Guy Hershenbaum <hershi@fb.com>
2023-12-16 11:12:57 +0200

    Add direct include needed for absl::StrCat
    Before this was used without a direct include statement, which relies on
    a transitive include to bring it into scope and can easily break...
    which it does with one of the more up-to-date versions of absl. This fix
    should help avoid breakages when we want to update the abseil
    dependency
    
    Repro:
    * Add the following to the WORKSPACE
    ```
    git_repository(
      name = "com_google_absl",
      remote = "https://github.com/abseil/abseil-cpp",
      commit = "27478af36914f18867ea32cb42db00dba15fffcd",
    )
    ```
    * bazel build src/main/cc/any_sketch:any_sketch - errors out

--- src/main/cc/any_sketch/any_sketch.cc
+++ src/main/cc/any_sketch/any_sketch.cc
@@ -25,6 +25,7 @@
 #include "absl/container/flat_hash_map.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "any_sketch/aggregators.h"
